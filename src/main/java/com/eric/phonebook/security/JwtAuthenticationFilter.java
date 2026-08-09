9bbecd0372e92f805ceca3f490463e62f589c247
Eric Vieira <ericvieira.p@outlook.com>
2026-08-09 18:37:48 -0300

    feat: starting jwtauthenticationfilter implementation

--- src/main/java/com/eric/phonebook/security/JwtAuthenticationFilter.java
+++ src/main/java/com/eric/phonebook/security/JwtAuthenticationFilter.java
@@ -1,5 +1,34 @@
 package com.eric.phonebook.security;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	
+	private final JwtService jwtService;
+	
+	private final CustomUserDetailsService userDetailsService;
+	
+	public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService userDetailsService) {
+		this.jwtService = jwtService;
+		this.userDetailsService = userDetailsService;
+	}
+
+
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
