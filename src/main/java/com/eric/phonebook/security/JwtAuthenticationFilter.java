c6cf345fc28d69600f29987f03d3bfdf939d4b67
Eric Vieira <ericvieira.p@outlook.com>
2026-08-09 19:32:48 -0300

    fix: finishing jwtauthenticationfilter

--- src/main/java/com/eric/phonebook/security/JwtAuthenticationFilter.java
+++ src/main/java/com/eric/phonebook/security/JwtAuthenticationFilter.java
@@ -2,6 +2,10 @@ package com.eric.phonebook.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -27,7 +31,58 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		String authorizationHeader = request.getHeader("Authorization");
+		
+		if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+			
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		String token = authorizationHeader.substring(7);
+		
+		try {
+			
+			String username = jwtService.extractUsername(token);
+			
+			if (username != null && SecurityContextHolder
+					.getContext()
+					.getAuthentication() == null) {
+				
+				UserDetails userDetails = userDetailsService
+						.loadUserByUsername(username);
+				
+				if (jwtService.isTokenValid(token, userDetails.getUsername())) {
+					
+					UsernamePasswordAuthenticationToken authentication = 
+							new UsernamePasswordAuthenticationToken(
+									userDetails,
+									null,
+									userDetails.getAuthorities()
+									);
+					
+					authentication.setDetails(
+							new WebAuthenticationDetailsSource()
+								.buildDetails(request)
+							);
+					
+					SecurityContextHolder
+						.getContext()
+						.setAuthentication(authentication);
+					
+				}
+				
+			}
+			
+		} catch (Exception e) {
+			
+			// Token inválido ou expirado.
+            // A requisição seguirá sem autenticação.
+			
+		}
+		
+		filterChain.doFilter(request, response);
 		
 	}
 
