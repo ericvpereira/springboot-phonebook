daf0add7bf1a3304e587d8fd81e3af0ed70e4e09
Eric Vieira <ericvieira.p@outlook.com>
2026-08-10 18:32:52 -0300

    feat: authservice implementation

---
+++ src/main/java/com/eric/phonebook/dto/auth/LoginResponse.java
@@ -0,0 +1,22 @@
+package com.eric.phonebook.dto.auth;
+
+public class LoginResponse {
+	
+	private String token;
+	
+	public LoginResponse() {
+	}
+
+	public LoginResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
